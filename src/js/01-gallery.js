0c7b30592e52a9c8e85d9652edecf37c35903361
Sasha <aapreutes@gmail.com>
2023-04-11 20:51:36 +0300

    1ex

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// 1. Create markup
+const galleryItemsList = document.querySelector('.gallery');
+const gallery = createPicGallery(galleryItems);
 
-console.log(galleryItems);
+galleryItemsList.insertAdjacentHTML('beforeend', gallery);
+
+function createPicGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+   </a>
+    `;
+    })
+    .join('');
+}
+
+// 2. Delegation
+
+galleryItemsList.addEventListener('click', onPicClick);
+
+function onPicClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+
+// 3. SimpleLightbox implementation
+
+const newGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

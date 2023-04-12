ec29414a554d2dbd50ba06c26537831e52906507
Sasha <aapreutes@gmail.com>
2023-04-12 22:13:19 +0300

    fix3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+import throttle from 'lodash.throttle';
 import '../css/common.css';
 import '../css/03-feedback.css';
 
@@ -6,33 +7,38 @@ const textarea = document.querySelector('textarea');
 const input = document.querySelector('input');
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+let formData = {};
 
-populateTextarea();
+populateForm();
 
 form.addEventListener('submit', onFormSubmit);
-// textarea.addEventListener('input', onTextareaInput);
-
-form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
+form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-function populateTextarea() {
-  const savedFormData = localStorage.getItem(STORAGE_KEY);
-  if (savedFormData) {
-    parsedData = JSON.parse(savedFormData);
-    textarea.value = parsedData.message;
-    input.value = parsedData.email;
-    input;
-    console.log(parsedData);
+  if (textarea.value === '' || input.value === '') {
+    alert('Please fill the form!');
+  } else {
+    console.log(formData),
+      evt.currentTarget.reset(),
+      localStorage.removeItem(STORAGE_KEY),
+      (formData = {});
   }
 }
 
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд (32:55)
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedForm) {
+    if (savedForm.email) {
+      input.value = savedForm.email;
+    }
+    if (savedForm.message) {
+      textarea.value = savedForm.message;
+    }
+  }
+}

3708c9bc8c94dc063233b3a1c3a909e2cce0b344
Sasha <aapreutes@gmail.com>
2023-04-12 22:50:16 +0300

    add2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import '../css/common.css';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+  localStorage.setItem(KEY, currentTime.seconds);
+}
+
+const start = localStorage.getItem(KEY);
+player.setCurrentTime(start);
